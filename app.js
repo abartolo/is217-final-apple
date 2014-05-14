efc8960129d85c52d1e4be2b80fb865d95d61567
abartolo2 <abartolo2@gmail.com>
2014-05-14 00:52:54 -0400

    fixing repo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  * Module dependencies.
  */
 
-<<<<<<< HEAD
+
 var express = require('express')
   , http = require('http')
   , fs = require('fs')
@@ -55,41 +55,3 @@ require('./routes')(app);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
-=======
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-//app.get('/calc', routes.calcpage);
-app.get('/hw3qunit',routes.qunithw3);
-app.get('/calc',routes.calcpage);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-
->>>>>>> bc29d8e658499236e9584f60e35438c54a1e3886
